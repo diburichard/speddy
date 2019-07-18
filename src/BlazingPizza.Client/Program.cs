cf846dc2afdae0a81bf0e7dd708796251aa6e1cc
Elvis Richard Romero Anez <eromero@central.cotas.com>
2019-07-18 18:57:02 -0400

    firt ini

---
+++ src/BlazingPizza.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Globalization;
+
+namespace BlazingPizza.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("es-MX");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
---
+++ src/BlazingPizza.Shared/OrderWithStatus.cs
@@ -0,0 +1,75 @@
+﻿using BlazingPizza.ComponentsLibrary.Map;
+using System;
+using System.Collections.Generic;
+
+namespace BlazingPizza
+{
+    public class OrderWithStatus
+    {
+        public Order Order { get; set; }
+
+        public string StatusText { get; set; }
+
+        public List<Marker> MapMarkers { get; set; }
+
+        public static OrderWithStatus FromOrder(Order order)
+        {
+            // To simulate a real backend process, we fake status updates based on the amount
+            // of time since the order was placed
+            string statusText;
+            List<Marker> mapMarkers;
+            var dispatchTime = order.CreatedTime.AddSeconds(10);
+            var deliveryDuration = TimeSpan.FromMinutes(1); // Unrealistic, but more interesting to watch
+
+            if (DateTime.Now < dispatchTime)
+            {
+                statusText = "Recogiendo el pedido";
+                mapMarkers = new List<Marker>
+                {
+                    ToMapMarker("Lugar de recogida", order.DeliveryLocation, showPopup: true)
+                };
+            }
+            else if (DateTime.Now < dispatchTime + deliveryDuration)
+            {
+                statusText = "Realizando el envio";
+
+                var startPosition = ComputeStartPosition(order);
+                var proportionOfDeliveryCompleted = Math.Min(1, (DateTime.Now - dispatchTime).TotalMilliseconds / deliveryDuration.TotalMilliseconds);
+                var driverPosition = LatLong.Interpolate(startPosition, order.DeliveryLocation, proportionOfDeliveryCompleted);
+                mapMarkers = new List<Marker>
+                {
+                    ToMapMarker("Lugar de entrega", order.DeliveryLocation),
+                    ToMapMarker("Mensajero", driverPosition, showPopup: true),
+                };
+            }
+            else
+            {
+                statusText = "Terminado";
+                mapMarkers = new List<Marker>
+                {
+                    ToMapMarker("Lugar de entrega", order.DeliveryLocation, showPopup: true),
+                };
+            }
+
+            return new OrderWithStatus
+            {
+                Order = order,
+                StatusText = statusText,
+                MapMarkers = mapMarkers,
+            };
+        }
+
+        private static LatLong ComputeStartPosition(Order order)
+        {
+            // Random but deterministic based on order ID
+            var rng = new Random(order.OrderId);
+            var distance = 0.01 + rng.NextDouble() * 0.02;
+            var angle = rng.NextDouble() * Math.PI * 2;
+            var offset = (distance * Math.Cos(angle), distance * Math.Sin(angle));
+            return new LatLong(order.DeliveryLocation.Latitude + offset.Item1, order.DeliveryLocation.Longitude + offset.Item2);
+        }
+
+        static Marker ToMapMarker(string description, LatLong coords, bool showPopup = false)
+            => new Marker { Description = description, X = coords.Longitude, Y = coords.Latitude, ShowPopup = showPopup };
+    }
+}
